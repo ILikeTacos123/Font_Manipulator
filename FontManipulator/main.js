15cae0eab2db9786edbcbacc4b15b4c40fdf49b6
ILikeTacos123 <75699754+ILikeTacos123@users.noreply.github.com>
2022-01-24 11:49:02 -0500

    Add files via upload

---
+++ FontManipulator/main.js
@@ -0,0 +1,42 @@
+var leftWrist_X;
+var rightWrist_X;
+var nose_x =0;
+var nose_y=0;
+actual_size = 0;
+function preload(){
+
+};
+function setup(){
+    Canvas=createCanvas(500 ,500);
+    Canvas.position(555 ,170);
+    video=createCapture(VIDEO);
+    video.position(40 ,120);
+    video.size(500 ,500);
+    poseNet=ml5.poseNet(video,modelloaded);
+    poseNet.on("pose",gotPoses);
+    };
+    function modelloaded(){
+        console.log("Model is loaded");
+        };
+
+        function draw(){
+            background(50, 190, 50);
+            fill("#89CFF0");
+            stroke("#89CFF0");
+            text("Ahir",nose_x,nose_y);
+            textSize(actual_size);
+
+        };
+function gotPoses(results){ 
+    if(results.length > 0){
+    console.log(results);
+    leftWrist_X=results[0].pose.leftWrist.x;
+    rightWrist_X=results[0].pose.rightWrist.x;
+    nose_x=results[0].pose.nose.x;
+    nose_y=results[0].pose.nose.y;
+actual_size=leftWrist_X - rightWrist_X;
+actual_size=floor(actual_size);
+console.log(actual_size);
+document.getElementById("height_n_width").innerHTML=actual_size;
+    };
+};
